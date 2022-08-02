62492f553d6de6532a5def9ada58fd764af58e2d
Anh Pham <anhpham@gmail.com>
2022-08-02 20:27:28 +0700

    feat: review homework

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,57 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var layoutButtonFacebook: View
+    private lateinit var imgButtonFacebook: ImageView
+    private lateinit var layoutButtonGoogle: View
+    private lateinit var imgButtonGoogle: ImageView
+    private lateinit var eTextName: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var tvTitle: TextView
+    private lateinit var tvLogin: TextView
+
+    //variable local : {} -> block code
+
+    //variable global :
+//    var iGlobal = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        initListener()
+        loadData()
+    }
 
-        val layoutButtonFacebook = findViewById<View>(R.id.layout_SignIn_Facebook)
-        val imgButtonFacebook = layoutButtonFacebook.findViewById<ImageView>(R.id.Btn_imageView)
+    private fun loadData() {
         imgButtonFacebook.setImageResource(R.drawable.facebook_icon)
-
-        val layoutButtonGoogle = findViewById<View>(R.id.layout_SignIn_Google)
-        val imgButtonGoogle = layoutButtonGoogle.findViewById<ImageView>(R.id.Btn_imageView)
         imgButtonGoogle.setImageResource(R.drawable.google_icon)
+    }
 
+    private fun initView() {
+        layoutButtonFacebook = findViewById(R.id.layout_SignIn_Facebook)
+        imgButtonFacebook = layoutButtonFacebook.findViewById(R.id.Btn_imageView)
+
+        layoutButtonGoogle = findViewById(R.id.layout_SignIn_Google)
+        imgButtonGoogle = layoutButtonGoogle.findViewById(R.id.Btn_imageView)
+
+        eTextName = findViewById(R.id.eText_Name)
+        btnLogin = findViewById(R.id.Btn_Login)
+        tvTitle = findViewById(R.id.Tv_Title2)
+        tvLogin = findViewById(R.id.Tv_Login)
+    }
+
+    private fun initListener() {
         layoutButtonFacebook.setOnClickListener {
             layoutButtonFacebook.isPressed = !layoutButtonFacebook.isPressed
         }
@@ -28,25 +60,42 @@ class MainActivity : AppCompatActivity() {
             layoutButtonGoogle.isPressed = !layoutButtonGoogle.isPressed
         }
 
-        val eTextName = findViewById<EditText>(R.id.eText_Name)
-
-        val btnLogin = findViewById<Button>(R.id.Btn_Login)
-        val tvTitle = findViewById<TextView>(R.id.Tv_Title2)
-        val tvLogin = findViewById<TextView>(R.id.Tv_Login)
         tvLogin.setOnClickListener {
-            if (eTextName.visibility == View.VISIBLE)
-            {
-                eTextName.visibility = View.INVISIBLE
-                tvLogin.text = "Sign up"
-                tvTitle.text = "Sign in to continue"
-                btnLogin.text = "Sign in"
-            }
-            else
-            {
+            //suggest
+//            val textLogin: Int
+//            val textTitle: String
+//            val textBtnLogin: Int
+//            val statusVisibility: Int
+//            when (eTextName.visibility) {
+//                View.VISIBLE -> {
+//                    textLogin = R.string.sign_up
+//                    textTitle = getString(R.string._in)
+//                    textBtnLogin = R.string.sign_in
+//                    statusVisibility = View.INVISIBLE
+//                }
+//                else -> {
+//                    textLogin = R.string.sign_in
+//                    textTitle = getString(R.string._up)
+//                    textBtnLogin = R.string.sign_up
+//                    statusVisibility = View.VISIBLE
+//                }
+//            }
+//            eTextName.visibility = statusVisibility
+//            tvLogin.text = getString(textLogin)
+//            tvTitle.text = getString(R.string.continue_format, textTitle)
+//            btnLogin.text = getString(textBtnLogin)
+
+            if (eTextName.visibility == View.VISIBLE) {
+                eTextName.visibility = View.GONE
+//                eTextName.visibility = View.INVISIBLE
+                tvLogin.text = getString(R.string.sign_up)
+                tvTitle.text = getString(R.string.continue_format, "in")
+                btnLogin.text = getString(R.string.sign_in)
+            } else {
                 eTextName.visibility = View.VISIBLE
-                tvLogin.text = "Sign in"
-                tvTitle.text = "Sign up to continue"
-                btnLogin.text = "Sign up"
+                tvLogin.text = getString(R.string.sign_in)
+                tvTitle.text = getString(R.string.continue_format, "up")
+                btnLogin.text = getString(R.string.sign_up)
             }
         }
     }
