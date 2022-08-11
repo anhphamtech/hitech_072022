ee8bf65e21ca02a358b1d00546f42df34d9c1fd6
Anh Pham <anhpham@gmail.com>
2022-08-11 16:34:21 +0700

    feat: buoi 9

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,134 +3,72 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
-    //Ca 2 deu duoc
-    var i123 = 0
-    var i1: Int = 0
-    var i2: Int? = null
-//    lateinit var i3 : Int
-
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_framelayout)
-        var user = User()
-        //hot ket : Shift + F6 -> change all project
-        user.fullName
-        user.age
-        sum(i123, i1)
-        sum(i1, i2)
-        i2?.let {
-            sum(i1, it)
-        }
-//        if(i2 !=null){
-//            sum(i1, i2!!)
-//        }
-
-        printName()
-        printName("Phuong")
-        printName("Vu")
-
-        demoFor()
-    }
-
-    fun sum(a: Int, b: Int?): Int {
-        //1
-        return when (b != null) {
-            true -> a + b
-            else -> a
-        }
-
-        //2
-//        return if (b != null)
-//            a + b
-//        else a
-    }
-
-//    fun sumShort(a: Int, b: Int) = a + b
-
-    private fun printName(name: String = "A") {
-
-        val firstChar = name.substring(0, 1)
-//        Log.d("hitechUni", firstChar.uppercase())
-
-        //1
-        //Search the same word in class -> Alt + J
-        var fullName = ""
-        when (firstChar) {
-            "P" -> fullName = "Phuong"
-            "V" -> fullName = "Vu"
-            else -> fullName = "Anh"
-        }
-
-        //2
-        val name2 = when (firstChar) {
-            "P" -> "Phuong"
-            "V" -> "Vu"
-            else -> "Anh"
-        }
-
-        Log.d("hitechUni", "$fullName - ${name2}2")
-    }
-
-    private fun demoFor() {
-        //1
-        var list1 = ArrayList<Int>()
-        var list2 = ArrayList<String>()
-        var list3 = ArrayList<Double>()
-        var list4 = ArrayList<User>()
-        //2
-        var list = arrayListOf<Int>()
-
-//        (i =11;i<20;i++)
-
-        for (i in 0..10) {
-            list.add(i)
-        }
-
-        for (i in 11 until 20) {
-            list.add(i)
-        }
-
-        list.remove(8)
-        list.removeAt(6)
-
-        //List
-        /* Index
-        * Start 0
-        * */
-
-        /* Size
-        * Start 1
-        * */
-        list.size
-
-        //No need index
-        list.forEach { item ->
-            Log.d("demoFor", "demoFor : $item")
-        }
-
-        list4.forEach {
-            it.fullName
-            it.age
-        }
-
-        //Need index for logic
-        for (index in 0 until list.size) {
-            Log.d("demoFor", "demoFor index : ${list[index]}")
-        }
+        setContentView(R.layout.activity_main)
+        initView()
+        initViewPager()
+        initListener()
 
     }
 
-    //1
-    fun noVoid(): Unit {
-
+    private fun initView() {
+        tabLayout = findViewById(R.id.actMain_tabLayout)
+        viewPager = findViewById(R.id.actMain_viewPager)
     }
 
-    //2
-    fun noVoid1(){
+    private fun initListener() {
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
 
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
+        tabLayout.setupWithViewPager(viewPager)
+
+//        tabLayout.getTabAt(1)?.select()
+//        viewPager.currentItem = 1
+
+        //index : start = 0
+        //size/length: start = 1
     }
+
+    private fun initViewPager() {
+        val adapterViewPager = ViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter = adapterViewPager
+    }
+
 }
---
+++ MyApplication/app/src/main/java/com/example/myapplication/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.myapplication.fragment.AgeFragment
+import com.example.myapplication.fragment.InfoFragment
+import com.example.myapplication.fragment.NameFragment
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+    override fun getCount() = 3
+
+    override fun getItem(position: Int) = when (position) {
+        0 -> NameFragment()
+        1 -> AgeFragment()
+        else -> InfoFragment()
+    }
+
+//    override fun getItem(position: Int): Fragment {
+//        return when (position) {
+//            0 -> NameFragment()
+//            else -> AgeFragment()
+//        }
+//    }
+}
